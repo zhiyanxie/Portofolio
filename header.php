46701264e81b02a8d711125cc52cae3448d2c018
zhiyanxie <xiezhiyanhfut@hotmail.com>
2015-08-27 18:28:00 -0500

    init

---
+++ header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+
+<html lang="en">
+<head>
+<meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title><?php echo $meta_title;?></title>
+<!-- style -->
+<link rel="stylesheet" href="css/styles.css">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+
+</head>
+
+
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php 
+
+
+    $meta_title = "Zhiyan Xie | Web Developer";
+    include_once("header.php") ;
+
+?>
+
+
+<body>
+
+<!-- nav-->
+<?php include_once("navigation.php")?>
+
+
+<div id="main">
+<section id="about">
+about
+about
+</section>
+
+<section id="work">
+work
+work
+</section>
+
+<section id="contact">
+contact
+contact
+</section>
+
+</div>
+</body>
+</html>
