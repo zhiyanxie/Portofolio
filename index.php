9e94a331fea98e3b0c30cd9659a303fcfcd33c0c
zhiyanxie <xiezhiyanhfut@hotmail.com>
2015-09-02 20:59:36 -0500

    add information

--- index.php
+++ index.php
@@ -23,9 +23,8 @@
             <source src="vedio/clouds.mp4" type="video/mp4">
      </video>
      <div class="intro">
-         <p>Think. Create. Design</p>
-         <p>ZHIYAN XIE</p>
-         <p>FRONT-END-DEVELOPER,UX DESIGNER</p>
+         <p class="about_title">Think. Create. Design</p>
+         <p class="about_subtitle">FRONT-END-DEVELOPER,UX DESIGNER</p>
      </div>
 </div>
 
@@ -36,24 +35,34 @@
 <section id="work">
 
 	<h1 class="heading">
-			<span>Work & Projects</span>
+			<span class="title">Work</span>
 			<span class="subtitle">A showcase of design</span>
 			<span class="subtitle">development, experiments and more</span>
 	</h1>
 <div class="container">
    <div class="row">
-     
-    <div class="col-md-3 one project" style="background-color:lavender;">Machine Learning</div>
-    <div class="col-md-9 two project" style="background-color:lavenderblush;"> Boring Online Game</div>
-</div>
-<div class="row projects">
-    <div class="col-md-6 three project" style="background-color:lavender;">Vedio Compression</div>
-    <div class="col-md-6 four project" style="background-color:lavenderblush;">RSA implementation</div>
-</div>
-<div class="row projects">
-    <div class="col-md-4 five project" style="background-color:lavender;">Vedio Compression</div>
-    <div class="col-md-4 six project" style="background-color:lavenderblush;">RSA implementation</div>
-    <div class="col-md-4 seven project" style="background-color:lavender;">RSA implementation</div>
+    <ul class="list-unstyled">
+      <li class="col-md-3  col-sm-12 project" style="background-color:lavender;">
+        <a href="ml">
+              <div class="one">
+						<div class="description">
+								<h4>Machine Learning</h4>
+								<span class="snippet">Analyzed patient data with Relative Comparisons (SVM) in Matlab.</span>
+								<span class="link">View Case Study</span>
+					    </div>
+			</div>
+        </a>
+      </li>
+      <li class="col-md-9 col-sm-12 two project" style="background-color:lavenderblush;"> Boring Online Game</li>
+
+ 
+      <li class="col-md-6 col-sm-12 three project" style="background-color:lavender;">Vedio Compression</li>
+      <li class="col-md-6 col-sm-12 four project" style="background-color:lavenderblush;">RSA implementation</li>
+       
+       <li class="col-md-4 col-sm-12 five project" style="background-color:lavender;">Vedio Compression</li>
+       <li class="col-md-4 col-sm-12 six project" style="background-color:lavenderblush;">RSA implementation</li>
+       <li class="col-md-4 col-sm-12 seven project" style="background-color:lavender;">RSA implementation</li>
+   </ul>
 </div>
 </div>
 </section>
@@ -61,31 +70,36 @@
 <!---resume_github-->
 <section id="resume_github">
  <h1 class="heading">
-			<span>Information</span>
+			<span class="title">Information</span>
 			<span class="subtitle">Know more about me</span>
 			<span class="subtitle">I love coding and perfecting pixels,things I would love to share </span>
  </h1>
-   <div class="row">
-      <div class="col-md-6">
-        <h2 class="center-x"> Resume </h1>
-      </div>
-      <div class="col-md-6">
-         <h2 class="center-x">Github</h1>
-      </div>
-   </div>
+  <div class="container">
+        <div class="component">
+        <a href="#" class="icon icon-cube facebook">facebook</a>
+        <a href="#" class="icon icon-cube twitter">twitter</a>
+        <a href="#" class="icon icon-cube googleplus">google+</a>
+        <a href="#" class="icon icon-cube github">github</a>
+        </div>
+        
+     </div>
+  
+  
 </section>
 
 <!-- contact -->
 <section id="contact">
 <h1 class="heading">
-			<span>Contact</span>
+			<span class="title">Contact</span>
 			<span class="subtitle">Currently available for hire</span>
 			<span class="subtitle">Don't hesitate,Would love to hear from you</span>
 	</h1>
-contact
-contact
-</section>
+  <!-- email-->
+ 
+   <!--icon-->
+   
 
+</section>
 
 </body>
 </html>
